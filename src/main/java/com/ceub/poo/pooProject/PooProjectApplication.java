b138adedfea7fc81b25bab80920b46735f40315e
Gabriel Gomes <gabriel.gomes@metareports.com.br>
2024-04-30 11:19:52 -0300

    Project initialized

---
+++ src/main/java/com/ceub/poo/pooProject/PooProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ceub.poo.pooProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ceub/poo/pooProject/PooProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceub.poo.pooProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
